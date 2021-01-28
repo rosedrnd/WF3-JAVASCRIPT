4c1e029e105f5aa6fdbc208f6f28e4021ef6c2b6
= <=>
2021-01-28 10:55:27 +0100

    add correction exercice function

--- exo/exercice-function.js
+++ exo/exercice-function.js
@@ -1,23 +1,32 @@
 window.onload = function () {
-    function createTags(balise1, balise2, repeat, array) {
-        var newArray = [];
+    function createTags(balise1, repeat) {
         for (let i = 1; i <= repeat; i++) {
-            newArray[i] = document.createElement(balise1);
-            newArray[i].setAttribute('class', 'color' + i);
-            newArray[i].setAttribute('id', 'div' + i);
+            var elementHtml = document.createElement(balise1);
+            elementHtml.setAttribute('class', `${'color' + i} square`);
+            elementHtml.setAttribute('id', 'div' + i);
             var body = document.querySelector('body');
-            body.appendChild(newArray[i]);
+            body.appendChild(elementHtml);
         }
-        var newArray2 = [];
-        
-        for (let i = 1, k = 0; i <= repeat && k <= repeat; i++, k++) {
-            newArray2[i] = document.createElement(balise2);
-            newArray2[i].setAttribute('class', 'paragraph' + i);
-            newArray[i].appendChild(newArray2[i]);
-            newArray2[i].innerText = array[k];
+        // console.log(newArray)
+    }
+
+    function createChildParagraphe(array){
+        var pTagNameHtml = 'p'
+        for (var i = 1; i <= array.length; i++) {
+            // Incrémentation de la div pour récupérer l'id de chaque div
+            divClassNameHtml = 'div' + i;
+            divElementHtml =  document.querySelector(`#${divClassNameHtml}`) 
+            // Création de l'élément paragraphe html
+            pElementHtml = document.createElement(pTagNameHtml);
+            // On commnence la boucle a 1 il faut faire -1 pour qu'on arrive à récupérer tous le tableau
+            pElementHtml.innerText = array[i-1]; 
+            pElementHtml.setAttribute('class', 'paragraph' + i);
+
+            divElementHtml.appendChild(pElementHtml);
         }
     }
     array = ["Helene", "Cathy", "Charlene"];
-    console.log(createTags('div', 'p', 3, array));
+    createTags('div', 3)
+    createChildParagraphe(array)
 }
 
