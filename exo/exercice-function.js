6f7cf26a96f2779d70f51a904f63bf0402b18278
Catherine Rose <catherinerosedurand@gmail.com>
2021-01-28 11:11:08 +0100

    changes

--- exo/exercice-function.js
+++ exo/exercice-function.js
@@ -1,22 +1,23 @@
 window.onload = function () {
     function createTags(balise1, balise2, repeat, array) {
-        var newArray = [];
         for (let i = 1; i <= repeat; i++) {
             newArray[i] = document.createElement(balise1);
-            newArray[i].setAttribute('class', 'color' + i);
+            newArray[i].setAttribute('class', 'color'+i);
             newArray[i].setAttribute('id', 'div' + i);
             var body = document.querySelector('body');
             body.appendChild(newArray[i]);
-        }
-        var newArray2 = [];
-        
-        for (let i = 1, k = 0; i <= repeat && k <= repeat; i++, k++) {
-            newArray2[i] = document.createElement(balise2);
-            newArray2[i].setAttribute('class', 'paragraph' + i);
-            newArray[i].appendChild(newArray2[i]);
-            newArray2[i].innerText = array[k];
+       
         }
     }
+
+}
+var newArray2 = [];
+
+for (let i = 1, k = 0; i <= repeat && k <= repeat; i++, k++) {
+    newArray2[i] = document.createElement(balise2);
+    newArray2[i].setAttribute('class', 'paragraph' + i);
+    newArray[i].appendChild(newArray2[i]);
+    newArray2[i].innerText = array[k];
     array = ["Helene", "Cathy", "Charlene"];
     console.log(createTags('div', 'p', 3, array));
 }
