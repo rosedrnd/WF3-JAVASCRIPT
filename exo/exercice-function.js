517ed9f5445cc6de56dc76a2638b4263c30944ec
Catherine Rose <catherinerosedurand@gmail.com>
2021-01-28 09:12:59 +0100

    optimisation of code

--- exo/exercice-function.js
+++ exo/exercice-function.js
@@ -1,5 +1,5 @@
 window.onload = function () {
-    function createTags(balise1, balise2, repeat) {
+    function createTags(balise1, balise2, repeat, array) {
         var newArray = [];
         for (let i = 1; i <= repeat; i++) {
             newArray[i] = document.createElement(balise1);
@@ -9,14 +9,15 @@ window.onload = function () {
             body.appendChild(newArray[i]);
         }
         var newArray2 = [];
-        let nameArray = ["Helene", "Cathy", "Charlene",];
+        
         for (let i = 1, k = 0; i <= repeat && k <= repeat; i++, k++) {
             newArray2[i] = document.createElement(balise2);
             newArray2[i].setAttribute('class', 'paragraph' + i);
             newArray[i].appendChild(newArray2[i]);
-            newArray2[i].innerText = nameArray[k];
+            newArray2[i].innerText = array[k];
         }
     }
-    console.log(createTags('div', 'p', 3));
+    array = ["Helene", "Cathy", "Charlene"];
+    console.log(createTags('div', 'p', 3, array));
 }
 
